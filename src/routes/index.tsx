85c42d97855424c2454f3a5056b75a9c4d40f692
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2022-09-12 22:55:55 -0300

    Feature: Adicionado React Router DOM

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,10 @@
-export { };
+import { Navigate, Route, Routes } from "react-router-dom";
+
+export const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<div>Home</div>} />
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  );
+};
